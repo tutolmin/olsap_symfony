fa6171b17724eba84a6177ff0475bbcef0954d48
Andrei Tutolmin <andrei@olsap.tutolmin.com>
2024-06-13 07:55:14 +0300

    TaskTechs form addition

--- tests/Controller/TaskTechsControllerTest.php
+++ tests/Controller/TaskTechsControllerTest.php
@@ -4,6 +4,8 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\TasksRepository;
+use App\Entity\Tasks;
 use App\Entity\TaskTechs;
 use App\Repository\TaskTechsRepository;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -24,6 +26,12 @@ class TaskTechsControllerTest extends WebTestCase
 
     /**
      * 
+     * @var TasksRepository
+     */
+    private $tasksRepository;
+
+    /**
+     * 
      * @var KernelBrowser
      */
     private $client;
@@ -33,6 +41,7 @@ class TaskTechsControllerTest extends WebTestCase
         $this->client = static::createClient();
         $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
         $this->taskTechsRepository = $this->entityManager->getRepository(TaskTechs::class);
+        $this->tasksRepository = $this->entityManager->getRepository(Tasks::class);
     }
 
     /**
@@ -117,4 +126,46 @@ class TaskTechsControllerTest extends WebTestCase
             $this->assertNull($removed_task);            
         }
     }
+
+    /**
+     * 
+     * @return Tasks
+     */
+    private function addDummyTask(): Tasks {
+            
+        $task = new Tasks();
+        $task->setName($this->dummy['name']);
+        $task->setPath($this->dummy['path']);
+        $this->assertTrue($this->tasksRepository->add($task, true));
+        
+        return $task;
+    }
+
+    /**
+     * 
+     * @return void
+     */
+    public function testCanAddDummyTaskTechBySubmittingForm(): void {
+        
+        $task = $this->addDummyTask();
+        $this->assertNotNull($task);
+
+        $crawler = $this->client->request('GET', '/tasks/techs/new');
+
+        // select the button
+        $buttonCrawlerNode = $crawler->selectButton('Save');
+
+        // retrieve the Form object for the form belonging to this button
+        $form = $buttonCrawlerNode->form();
+        $tech = $form->get('tech')->getValue();
+        
+        // set values on a form object
+        $form['task_techs[task]'] = $task->getId();
+
+        // submit the Form object
+        $this->client->submit($form);
+        
+        $item = $this->taskTechsRepository->findOneByPath($this->dummy['path']);
+        $this->assertNotNull($item);        
+    }    
 }
